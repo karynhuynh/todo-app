1069b57934ce41cb60921ec9b238e719ef07d2a5
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-07-21 20:03:31 -0700

    Added js to remove a todo when the trash button was clicked

--- src/script.js
+++ src/script.js
@@ -1,12 +1,11 @@
-// selectors
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-// event listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
-// functions
+// adding the to do
 function addTodo(event) {
   // prevents form from submitting/ refreshing
   event.preventDefault();
@@ -15,7 +14,7 @@ function addTodo(event) {
   todoDiv.classList.add("todo");
   // create li -> child element
   const newTodo = document.createElement("li");
-  newTodo.innerText = "hey";
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
   // checkmark button -> child element
@@ -30,4 +29,23 @@ function addTodo(event) {
   todoDiv.appendChild(trashButton);
   // append to list -> child element
   todoList.appendChild(todoDiv);
+  // clear todo input value
+  todoInput.value = "";
+}
+
+// delete to do
+todoList.addEventListener("click", deleteCheck);
+
+function deleteCheck(event) {
+  const item = event.target;
+  // delete to do by clicking on the trash button
+  if (item.classList[0] === "trash-button") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+  // marking a to do as complete
+  if (item.classList[0] === "complete-button") {
+    const todo = item.parentElement;
+    todo.classList.toogle("completed");
+  }
 }
