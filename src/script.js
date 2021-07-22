76fd4448c4ddc0ddf5e2ff518cdd675ac5d74c5d
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-07-21 19:40:27 -0700

    Added css to the to do parent and child elements

--- src/script.js
+++ src/script.js
@@ -10,24 +10,24 @@ todoButton.addEventListener("click", addTodo);
 function addTodo(event) {
   // prevents form from submitting/ refreshing
   event.preventDefault();
-  // to do div
+  // to do div -> parent element
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
-  // create li
+  // create li -> child element
   const newTodo = document.createElement("li");
   newTodo.innerText = "hey";
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
-  // checkmark button
+  // checkmark button -> child element
   const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
   completedButton.classList.add("complete-button");
   todoDiv.appendChild(completedButton);
-  // trash button
+  // trash button -> child element
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("complete-button");
+  trashButton.classList.add("trash-button");
   todoDiv.appendChild(trashButton);
-  // append to list
+  // append to list -> child element
   todoList.appendChild(todoDiv);
 }
